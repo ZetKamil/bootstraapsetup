08156144d9a1e1a9a0388cda5f2bdd55076d53b5
ZetKamil <kamil.ziemkiewicz.97@gmail.com>
2025-11-18 11:08:29 +0100

    eerste

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import {resolve} from 'path'
-export default{
-    root:resolve(__dirname,'src'),
-    build:{
-        outDir:'../dist'
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    base: './',
+    root: resolve(__dirname, 'src'),
+    build: {
+        outDir: '../dist',
+        assetsDir: 'assets'
     },
-    base:'./',
-    server:{
-        port:8080
+    server: { port: 8080 },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                silenceDeprecations: ['import','mixed-decls','color-functions','global-builtin']
+            }
+        }
     }
-}
+})
